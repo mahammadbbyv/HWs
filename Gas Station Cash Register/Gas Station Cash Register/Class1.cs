4a674aa46d9c282d14730056e330613cb595fb73
MagamedBabayev14 <rasimbabayev9g19@gmail.com>
2023-03-08 09:21:17 +0400

    HW2

---
+++ Gas Station Cash Register/Gas Station Cash Register/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gas_Station_Cash_Register
+{
+
+    public class Invoice
+    {
+        public bool paid { get; set; } = true;
+        public string invoice_id { get; set; }
+        public DateTime invoice_date { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime invoice_dateDue { get; set; } = DateTime.Now.ToLocalTime();
+        public string issuer_name { get; set; } = "Maga";
+        public string issuer_company { get; set; } = "Maga's Fuel";
+        public string issuer_address { get; set; } = "Necefqulu Refiyev kuechesi, 14";
+        public string issuer_website { get; set; } = "telegram-bots-maga.cx.ua";
+        public string issuer_email { get; set; } = "magasfuel@gmail.com";
+        public string client_name { get; set; }
+        public string client_company { get; set; } = "";
+        public string client_address { get; set; } = "";
+        public string client_email { get; set; }
+        public List<Item> items { get; set; } = new();
+        public int discount { get; set; } = 0;
+        public int tax { get; set; } = 0;
+        public int total { get; set; }
+    }
+
+    public class Item
+    {
+        public string name { get; set; }
+        public float price { get; set; }
+    }
+
+}
