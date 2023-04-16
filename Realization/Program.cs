c812547c32fe24007724f4ef47633413ab3eb0b8
MagaB16 <rasimbabayev9g19@gmail.com>
2023-04-16 10:49:04 +0400

    test

---
+++ Realization/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Create_Character.Model;
+#pragma warning disable CS8601
+
+Character character = new();
+
+Console.WriteLine("Enter character's name: ");
+character.Name = Console.ReadLine();
+Console.WriteLine("Enter character's power: ");
+character.Power = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter character's group: ");
+character.Group = Console.ReadLine();
+
+#pragma warning restore CS8601
