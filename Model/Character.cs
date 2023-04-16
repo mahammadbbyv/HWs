de9f1d6285a9a1903243289211d32a6536741c7a
MagaB16 <rasimbabayev9g19@gmail.com>
2023-04-16 10:50:22 +0400

    test

---
+++ Model/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Character.Model
+{
+    public class Character
+    {
+#pragma warning disable CS8618
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public string Group { get; set; }
+#pragma warning restore CS8618
+    }
+}
