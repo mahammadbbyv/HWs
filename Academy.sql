059ad3c1c558fb95b9f393af505f7189e05ec98a
MagaB16 <rasimbabayev9g19@gmail.com>
2023-04-24 23:32:49 +0400

    New HW

---
+++ Academy.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Groups (
+    Id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [Name] nvarchar(10) NOT NULL UNIQUE,
+    Rating int NOT NULL CHECK (Rating >= 0 AND Rating <= 5),
+    [Year] int NOT NULL CHECK (Year >= 1 AND Year <= 5)
+);
+
+CREATE TABLE Departments (
+    Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    Financing money NOT NULL DEFAULT 0,
+    [Name] nvarchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Faculties (
+  Id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  [Name] nvarchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Teachers (
+  Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  EmploymentDate date NOT NULL CHECK (EmploymentDate >= '1990-01-01'),
+  [Name] nvarchar(max) NOT NULL CHECK (Name <> ''),
+  Premium money NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+  Salary money NOT NULL CHECK (Salary > 0),
+  Surname nvarchar(max) NOT NULL CHECK (Surname <> '')
+);
