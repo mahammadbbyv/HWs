54444eae8d61f3ae924d96be0d0136f7eacaac75
MagaB16 <rasimbabayev9g19@gmail.com>
2023-07-24 22:58:28 +0400

    HW

---
+++ TcpListener/TcpClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+#pragma warning disable CS8604
+string ip = Console.ReadLine();
+int port = Convert.ToInt32(Console.ReadLine());
+try
+{
+    while (true)
+    {
+        TcpClient client = new TcpClient();
+        client.Connect(IPAddress.Parse(ip), port);
+        Console.WriteLine($"Connected to 127.0.0.1:8080");
+        string? message = Console.ReadLine();
+        byte[] data = Encoding.ASCII.GetBytes(message);
+
+        NetworkStream stream = client.GetStream();
+        stream.Write(data, 0, data.Length);
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Server Response: {response}");
+        client.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+#pragma warning restore CS8604
---
+++ TcpListener/TcpListener/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+try
+{
+    IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    int port = 8080;
+    TcpListener listener = new TcpListener(ipAddress, port);
+    listener.Start();
+    Console.WriteLine($"TCP Listener started on {ipAddress}:{port}");
+
+    while (true)
+    {
+        TcpClient client = listener.AcceptTcpClient();
+        HandleClient(client);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+static void HandleClient(TcpClient client)
+{
+    try
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Received: {data}");
+        string responseMessage = Console.ReadLine();
+        byte[] responseData = Encoding.ASCII.GetBytes(responseMessage);
+        stream.Write(responseData, 0, responseData.Length);
+        client.Close();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
