913e4d72ffe36b4ba530754b78a16be796d1e6cd
MagaB16 <rasimbabayev9g19@gmail.com>
2023-10-13 17:06:19 +0400

    0.1v

--- API/index.js
+++ API/index.js
@@ -5,54 +5,72 @@ const app = express();
 const PORT = 3000;
 const fs = require('fs');
 app.use(bodyParser.json());
-function between(min, max) {  
+function between(min, max) {
   return Math.floor(
     Math.random() * (max - min) + min
   )
 }
 app.get('/getWords', (req, res) => {
-  const exists = fs.existsSync(`${req.query.fileName}.json`);
-  
-  if (exists) {
-    if(req.query.randomWord){
-      fs.readFile(`${req.query.fileName}.json`, "utf-8", (err, data) => {
-        var result = JSON.parse(data);
-        let response = {ok: "true", result: result[between(0, result.length)]};
-        res.write(JSON.stringify(response));
-        res.end();
-      });}
+  if(req.query.password == "e67f14fd15c493b31d7e3b0d0df58be8")
+  {
+    const exists = fs.existsSync(`${req.query.fileName}.json`);
+    if (exists) {
+      if(req.query.randomWord){
+        fs.readFile(`${req.query.fileName}.json`, "utf-8", (err, data) => {
+          var result = JSON.parse(data);
+          let response = {ok: "true", result: result[between(0, result.lengths)]};
+          res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+          res.write(JSON.stringify(response));
+          res.end();
+        });}
+      else{
+        fs.readFile(`${req.query.fileName}.json`, "utf-8", (err, data) => {
+          res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+          res.write(req.query.randomWord);
+          res.end();
+        });
+      }
+    }
     else{
-      fs.readFile(`${req.query.fileName}.json`, "utf-8", (err, data) => {
-        res.write(req.query.randomWord);
-        res.end();
-      });
+      let response = {ok: "false", reason: "There is no such library!"};
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      res.write(JSON.stringify(response));
+      res.end();
     }
   }
-  else{
-    let response = {ok: "false", reason: "There is no such library!"};
+  else
+  {
+    let response = {ok: "false", reason: "Wrong password!"};
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.write(JSON.stringify(response));
     res.end();
   }
 });
 
 app.get('/create', (req, res) => {
-  let result;
-  const exists = fs.existsSync("./reports/new.txt")
-  if(exists){
-      result = {ok: "false", reason: "Library with such name already exists!"};
+  if(req.query.password == "e67f14fd15c493b31d7e3b0d0df58be8"){
+    let result;
+    const exists = fs.existsSync("./reports/new.txt")
+    if(exists){
+        result = {ok: "false", reason: "Library with such name already exists!"};
+        res.write(JSON.stringify(result));
+        res.end();
+    } else {
+      fs.appendFile(`${req.query.fileName}.json`, req.query.content, function (err) {
+        if (err) {
+          result = {ok: "false"};
+          res.send(result)
+        };
+        console.log('Saved!');
+      });
+      
+      result = {ok: "true"};
       res.write(JSON.stringify(result));
       res.end();
-  } else {
-    fs.appendFile(`${req.query.fileName}.json`, req.query.content, function (err) {
-      if (err) {
-        result = {ok: "false"};
-        res.send(result)
-      };
-      console.log('Saved!');
-    });
-    
-    result = {ok: "true"};
-    res.write(JSON.stringify(result));
+  }
+  }else{
+    let response = {ok: "false", reason: "Wrong password!a"};
+    res.write(JSON.stringify(response));
     res.end();
   }
 });
