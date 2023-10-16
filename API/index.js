d7787593b87f53aed1e88655118786a07489b406
MagaB16 <rasimbabayev9g19@gmail.com>
2023-10-16 04:35:15 +0400

    last

--- API/index.js
+++ API/index.js
@@ -22,6 +22,15 @@ function findPack(arr, filename){
   }
 }
 
+function checkExists(arr, username){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].username == username){
+      return 1;
+    }
+  }
+  return 0;
+}
+
 app.get('/getWords', (req, res) => {
     const exists = fs.existsSync(`./${req.query.fileName}.json`);
     if (exists) {
@@ -42,7 +51,7 @@ app.get('/getWords', (req, res) => {
         });
         if(req.query.randomWord){
           file.words = JSON.parse(file.words);
-          let response = {ok: "true", result: file.words[between(0, file.words.length)]};
+          response = {ok: "true", result: file.words[between(0, file.words.length)]};
           console.log(file.words);
           res.writeHead(200, {'Content-Type': 'charset=utf-8'});
           res.write(JSON.stringify(response));
@@ -56,7 +65,7 @@ app.get('/getWords', (req, res) => {
       });
     }
     else{
-      let response = {ok: "false", reason: "There is no such library!"};
+      response = {ok: "false", reason: "There is no such library!"};
       res.writeHead(200, {'Content-Type': 'charset=utf-8'});
       res.write(JSON.stringify(response));
       res.end();
@@ -83,7 +92,7 @@ app.get('/getTopPacks', (req, res) => {
     // fs.readFile(`${req.query.fileName}.json`, "utf-8", (err, data) => {
     //   let file = JSON.parse(data);
     //   if(req.query.randomWord){
-    //     let response = {ok: "true", result: file[between(0, file.length)]};
+    //     response = {ok: "true", result: file[between(0, file.length)]};
     //     res.writeHead(200, {'Content-Type': 'charset=utf-8'});
     //     res.write(JSON.stringify(response));
     //     res.end();
@@ -98,7 +107,7 @@ app.get('/getTopPacks', (req, res) => {
 
 app.get('/createPack', (req, res) => {
   if(req.query.isLogged){
-    let result;
+    result;
     const exists = fs.existsSync(`./${req.query.fileName}.json`);
     console.log(exists);
     if(exists){
@@ -163,15 +172,15 @@ app.get('/createPack', (req, res) => {
     }
   }
   else{
-    let response = {ok: "false", reason: "You are not registered!"};
+    response = {ok: "false", reason: "You are not registered!"};
     res.write(JSON.stringify(response));
     res.end();
   }
 });
 
 app.get('/deletePack', (req, res) => {
-  let result;
-  const exists = fs.existsSync(`./${req.query.fileName}.json`)
+  result;
+  const exists = fs.existsSync(`./${req.query.fileName}.json`);
   if(!exists){
       result = {ok: "false", reason: "Library with such name does not exist!"};
       res.write(JSON.stringify(result));
@@ -222,7 +231,7 @@ app.get('/deletePack', (req, res) => {
       }
       else
       {
-        let response = {ok: "false", reason: "Wrong username or password!"};
+        response = {ok: "false", reason: "Wrong username or password!"};
         console.log(req.query.login + " " + req.query.password);
         console.log(file.login + " " + file.password);
 
@@ -233,6 +242,77 @@ app.get('/deletePack', (req, res) => {
     });
   }
 });
+
+app.get('/addUser', (req, res) => {
+  const exists = fs.existsSync(`./users.json`);
+  var result = {};
+  if(!exists){
+    var usernameRegexPattern =/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+    if(usernameRegexPattern.test(req.query.username)){
+      var passwordRegexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+      if(passwordRegexPattern.test(req.query.password)){
+        if(req.query.password == req.query.confirm){
+          let users = [{username: req.query.username, password: req.query.password}];
+          fs.appendFile(`./users.json`, JSON.stringify(users), function (err) {
+            if (err) {
+              result = {ok: "false"};
+              res.send(result)
+            };
+            console.log('User created addedd!');
+          });
+        }else{
+          result = {ok: false, reason: "Passwords do not match!"};
+        }
+      }else{
+      result = {ok: false, reason: "Password is not valid!"};
+    }
+    }else{
+      result = {ok: false, reason: "Username is not valid!"};
+    }
+  }else{
+    fs.readFile('IDs.json', "utf-8", (err, data) => {
+      if(!checkExists(JSON.parse(data), req.query.username)){
+        var usernameRegexPattern =/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+        if(usernameRegexPattern.test(req.query.username)){
+          var passwordRegexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+          if(passwordRegexPattern.test(req.query.password)){
+            if(req.query.password == req.query.confirm){
+              let users = JSON.parse(data);
+              users[users.length] = {username: req.query.username, password: req.query.password};
+              fs.unlink(`./users.json`, function (err) {
+                if (err) {
+                  result = {ok: "false"};
+                  res.send(result)
+                };
+                console.log('Deleted! users');
+              });
+              fs.appendFile(`./users.json`, JSON.stringify(users), function (err) {
+                if (err) {
+                  result = {ok: "false"};
+                  res.send(result);
+                };
+                console.log('User created addedd!');
+              });
+            }else{
+              result = {ok: false, reason: "Passwords do not match!"};
+            }
+          }
+          else{
+          result = {ok: false, reason: "Password is not valid!"};
+          }
+        }else{
+          result = {ok: false, reason: "Username is not valid!"};
+        }
+      }else{
+        result = {ok: false, reason: "Such username already exists!"};
+      }
+    });
+    
+  }
+  res.write(result);
+  res.end();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
